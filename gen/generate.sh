d8b583307e7d2231da2062d5293bd9b619f55e3c
tan <tanmaykm@gmail.com>
2016-10-23 07:29:32 +0530

    move base api to subfolder

--- gen/generate.sh
+++ gen/generate.sh
@@ -7,19 +7,23 @@ fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GENDIR="$( readlink -e "${DIR}/../" )"
+TEMPGENDIR=${GENDIR}/_tmp
 SPECDIR=$1
 SPECFILE=root_swagger.json
 SWAGGERDIR=`julia -e 'println(Pkg.dir("Swagger"))'`
 
 echo "Swagger is at $SWAGGERDIR"
-echo "Generating into $GENDIR"
-${SWAGGERDIR}/plugin/generate.sh -i ${SPECDIR}/${SPECFILE} -o ${GENDIR} -c ${DIR}/config.json
+echo "Generating into $TEMPGENDIR, moving into $GENDIR"
+mkdir -p ${TEMPGENDIR}
+${SWAGGERDIR}/plugin/generate.sh -i ${SPECDIR}/${SPECFILE} -o ${TEMPGENDIR} -c ${DIR}/config.json
+mv ${TEMPGENDIR}/src/* ${GENDIR}/src/api/
+rm ${TEMPGENDIR}/LICENSE
+rm -r ${TEMPGENDIR}
 
 # model for Patch is not generated because of https://github.com/swagger-api/swagger-codegen/issues/2856
 # add a hand coded module for now
-cp ${DIR}/model_unversioned_Patch.jl ${GENDIR}/src/
+cp ${DIR}/model_unversioned_Patch.jl ${GENDIR}/src/api/
 # the following models are not generated correctly by Swagger, hand code them for now
-cp ${DIR}/model_resource_Quantity.jl ${GENDIR}/src/
-cp ${DIR}/model_unversioned_Time.jl ${GENDIR}/src/
-cp ${DIR}/model_intstr_IntOrString.jl ${GENDIR}/src/
-rm ${GENDIR}/LICENSE
+cp ${DIR}/model_resource_Quantity.jl ${GENDIR}/src/api/
+cp ${DIR}/model_unversioned_Time.jl ${GENDIR}/src/api/
+cp ${DIR}/model_intstr_IntOrString.jl ${GENDIR}/src/api/
