e4d18ca4b9d31a9d103d61d8d221a8eb78f00594
tan <tanmaykm@gmail.com>
2016-10-25 23:53:35 +0530

    expose a much simpler api

--- gen/model_intstr_IntOrString.jl
+++ gen/model_intstr_IntOrString.jl
@@ -1,1 +1,3 @@
 typealias IntstrIntOrString String
+
+convert{T<:Integer}(::Type{IntstrIntOrString}, v::T) = string(v)
--- src/Kuber.jl
+++ src/Kuber.jl
@@ -5,131 +5,94 @@ using .Kubernetes
 using JSON
 using Swagger
 
-import Base: convert
+import Base: convert, get, put!
 import Swagger: SwaggerModel
 
-convert{T<:SwaggerModel}(T, json::String) = convert(T, JSON.parse(json))
+convert{T<:SwaggerModel}(::Type{T}, json::String) = convert(T, JSON.parse(json))
 
-function list_status_component(api)
-    res = listCoreV1ComponentStatus(api)
-    isset_field(res, "items") || return
-    for item in get_field(res, "items")
-        println(get_field(get_field(item, "metadata"), "name"), ": ", get_field(get_field(item, "conditions")[1], "type"))
+macro K_str(json)
+    quote
+        j = JSON.parse($json)
+        eval(Symbol(j["kind"]))(j)
     end
 end
 
-function status_component(api, compname)
-    item = readCoreV1ComponentStatus(api, compname)
-    println(get_field(get_field(item, "metadata"), "name"), ": ", get_field(get_field(item, "conditions")[1], "type"))
-end
+export @K_str
 
-function list_endpoints(api)
-    res = listCoreV1EndpointsForAllNamespaces(api)
-    isset_field(res, "items") || return
-    for item in get_field(res, "items")
-        metadata = get_field(item, "metadata")
-        name = get_field(metadata, "namespace") * "/" * get_field(metadata, "name")
-        addresses = ""
-        ports = ""
-        subsets = get_field(item, "subsets")
-        for subset in subsets
-            for addr in get_field(subset, "addresses")
-                addresses = addresses * " " * get_field(addr, "ip")
-            end
-            for port in get_field(subset, "ports")
-                ports = ports * " " * string(get_field(port, "port"))
-            end
-        end
-        println(name, " addresses(", addresses, ") ports(", ports, ")")
+##
+# default global context to make apis easy
+
+const DEFAULT_NAMESPACE = "default"
+const DEFAULT_URI = "http://localhost:8001/"
+
+type KuberContext
+    api::DefaultApi
+    namespace::String
+
+    function KuberContext()
+        new(DefaultApi(Swagger.Client(DEFAULT_URI)), DEFAULT_NAMESPACE)
     end
 end
 
-function list_namespaces(api)
-    res = listCoreV1Namespace(api)
-    isset_field(res, "items") || return
-    for item in get_field(res, "items")
-        metadata = get_field(item, "metadata")
-        name = get_field(metadata, "name")
-        println(name)
+const _global_ctx = KuberContext()
+
+function set_server(uri::String=DEFAULT_URI)
+    global _global_ctx
+    client = Swagger.Client(uri)
+    _global_ctx.api = DefaultApi(client)
+end
+
+function set_ns(namespace::String)
+    global _global_ctx
+    _global_ctx.namespace = namespace
+end
+
+# simpler names for frequent types
+typealias ComponentStatus           V1ComponentStatus
+typealias Endpoints                 V1Endpoints
+typealias Namespace                 V1Namespace
+typealias Pod                       V1Pod
+typealias PodTemplate               V1PodTemplate
+typealias ReplicationController     V1ReplicationController
+typealias Service                   V1Service
+
+typealias DeleteOptions             V1DeleteOptions
+typealias Preconditions             V1Preconditions
+
+for fn in (:get, :put!, :update!, :delete!)
+    @eval begin
+        $fn{T<:SwaggerModel}(::Type{T}, args...; kwargs...) = $fn(_global_ctx, T, args...; kwargs...)
+        $fn{T<:SwaggerModel}(t::T, args...; kwargs...) = $fn(_global_ctx, t, args...; kwargs...)
+
+        $fn{T<:SwaggerModel}(f::Function, ::Type{T}, args...; kwargs...) = f($fn(_global_ctx, T, args...; kwargs...))
+        $fn{T<:SwaggerModel}(f::Function, t::T, args...; kwargs...) = f($fn(_global_ctx, t, args...; kwargs...))
     end
 end
 
-function list_pods(api; namespace="default")
-    res = listCoreV1NamespacedPod(api, namespace)
-    isset_field(res, "items") || return
-    for item in get_field(res, "items")
-        metadata = get_field(item, "metadata")
-        name = get_field(metadata, "namespace") * "/" * get_field(metadata, "name")
-        println(name)
-    end
-end
+get(ctx::KuberContext, ::Type{ComponentStatus}) = listCoreV1ComponentStatus(ctx.api)
+get(ctx::KuberContext, ::Type{ComponentStatus}, compname::String) = readCoreV1ComponentStatus(ctx.api, compname)
+get(ctx::KuberContext, ::Type{Endpoints}) = listCoreV1EndpointsForAllNamespaces(ctx.api)
+get(ctx::KuberContext, ::Type{Namespace}) = listCoreV1Namespace(ctx.api)
+get(ctx::KuberContext, ::Type{Pod}) = listCoreV1NamespacedPod(ctx.api, ctx.namespace)
+get(ctx::KuberContext, ::Type{PodTemplate}) = listCoreV1NamespacedPodTemplate(ctx.api, ctx.namespace)
+get(ctx::KuberContext, ::Type{ReplicationController}) = listCoreV1NamespacedReplicationController(ctx.api, ctx.namespace)
+get(ctx::KuberContext, ::Type{Service}) = listCoreV1NamespacedService(ctx.api, ctx.namespace)
 
-function list_pod_templates(api; namespace="default")
-    res = listCoreV1NamespacedPodTemplate(api, namespace)
-    #isset_field(res, "items") || return
-    #for item in get_field(res, "items")
-    #    metadata = get_field(item, "metadata")
-    #    name = get_field(metadata, "namespace") * "/" * get_field(metadata, "name")
-    #    println(io, name)
-    #end
-end
+put!(ctx::KuberContext, ns::Namespace) = createCoreV1Namespace(ctx.api, ns)
+put!(ctx::KuberContext, pod::Pod) = createCoreV1NamespacedPod(ctx.api, ctx.namespace, pod)
+put!(ctx::KuberContext, pt::PodTemplate) = createCoreV1NamespacedPodTemplate(ctx.api, ctx.namespace, pt)
+put!(ctx::KuberContext, rc::ReplicationController) = createCoreV1NamespacedReplicationController(ctx.api, ctx.namespace, rc)
+put!(ctx::KuberContext, rc::Service) = createCoreV1NamespacedService(ctx.api, ctx.namespace, rc)
 
-function list_rcs(api; namespace="default")
-    api = listCoreV1NamespacedReplicationController(api, namespace)
-end
+_delopts(; kwargs...) = DeleteOptions(; preconditions=Preconditions(; kwargs...), kwargs...)
+delete!(ctx::KuberContext, ::Type{Namespace},  ns::String; kwargs...) = deleteCoreV1Namespace(ctx.api, nctx.amespace, _delopts(; kwargs...))
+delete!(ctx::KuberContext, ::Type{Pod}, pod::String; kwargs...) = deleteCoreV1NamespacedPod(ctx.api, pod, ctx.namespace, _delopts(; kwargs...))
+delete!(ctx::KuberContext, ::Type{PodTemplate}, pt::String; kwargs...) = deleteCoreV1NamespacedPodTemplate(ctx.api, pt, ctx.namespace, _delopts(; kwargs...))
+delete!(ctx::KuberContext, ::Type{ReplicationController}, rc::String; kwargs...) = deleteCoreV1NamespacedReplicationController(ctx.api, rc, ctx.namespace, _delopts(; kwargs...))
+delete!(ctx::KuberContext, ::Type{Service}, service::String) = deleteCoreV1NamespacedService(ctx.api, service, ctx.namespace)
 
-function list_services(api; namespace="default")
-    res = listCoreV1NamespacedService(api, namespace)
-end
-
-function create_namespace(api, namespace)
-    obj = convert(V1Namespace, namespace)
-    res = createCoreV1Namespace(api, obj)
-end
-
-function create_pod(api, pod; namespace="default")
-    obj = convert(V1Pod, pod)
-    res = createCoreV1NamespacedPod(api, namespace, obj)
-end
-
-function create_pod_template(api, template; namespace="default")
-    obj = convert(V1PodTemplate, template)
-    res = createCoreV1NamespacedPodTemplate(api, namespace, obj)
-end
-
-function create_rc(api, rc; namespace="default")
-    obj = convert(V1ReplicationController, rc)
-    res = createCoreV1NamespacedReplicationController(api, namespace, obj)
-end
-
-function create_service(api, service; namespace="default")
-    obj = convert(V1Service, rc)
-    res = createCoreV1NamespacedService(api, namespace, obj)
-end
-
-function delete_namespace(api, namespace, options="{}")
-    opts = convert(V1DeleteOptions, options)
-    res = deleteCoreV1Namespace(api, namespace, opts)
-end
-
-function delete_pod(api, pod; namespace="default", options="{}")
-    opts = convert(V1DeleteOptions, options)
-    res = deleteCoreV1NamespacedPod(api, pod, namespace, opts)
-end
-
-function delete_pod_template(api, template; namespace="default", options="{}")
-    opts = convert(V1DeleteOptions, options)
-    res = deleteCoreV1NamespacedPodTemplate(api, template, namespace, opts)
-end
-
-function delete_rc(api, rc; namespace="default", options="{}")
-    opts = convert(V1DeleteOptions, options)
-    res = deleteCoreV1NamespacedReplicationController(api, rc, namespace, opts)
-end
-
-function delete_service(api, service; namespace="default", options="{}")
-    opts = convert(V1DeleteOptions, options)
-    res = deleteCoreV1NamespacedService(api, service, namespace, opts)
-end
+export set_server, set_ns
+export ComponentStatus, Endpoints, Namespace, Pod, PodTemplate, ReplicationController, Service
+export get, put!, update!, delete!
 
 end # module
