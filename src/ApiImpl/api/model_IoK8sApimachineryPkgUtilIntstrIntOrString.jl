0630e8291f1e878816519dd8562f1c7429827fbf
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@juliacomputing.com>
2022-03-22 17:08:44 +0200

    Edit the file in  folder instead of the output

--- src/ApiImpl/api/model_IoK8sApimachineryPkgUtilIntstrIntOrString.jl
+++ src/ApiImpl/api/model_IoK8sApimachineryPkgUtilIntstrIntOrString.jl
@@ -1,9 +1,6 @@
 if !isdefined(@__MODULE__, :IoK8sApimachineryPkgUtilIntstrIntOrString)
     const IoK8sApimachineryPkgUtilIntstrIntOrString = String
+    convert(::Type{IoK8sApimachineryPkgUtilIntstrIntOrString}, v::T) where {T<:Integer} = string(v)
 else
     @warn("Skipping redefinition of IoK8sApimachineryPkgUtilIntstrIntOrString to String")
-end
-
-if !hasmethod(convert, (Type{IoK8sApimachineryPkgUtilIntstrIntOrString}, T where {T<:Integer}))
-    convert(::Type{IoK8sApimachineryPkgUtilIntstrIntOrString}, v::T) where {T<:Integer} = string(v)
 end
